7a2c12bf96813cbf67019591c96bd8ecd14e4af7
Krishna Chaitanya Posinaseetti <posinaseetti@gmail.com>
2025-10-26 09:15:04 +1100

    Added project structure and first test

---
+++ src/test/java/com/api/tests/MyFirstAPITest.java
@@ -0,0 +1,32 @@
+package com.api.tests;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class MyFirstAPITest {
+
+    @Test
+
+    public void testGetAllUsers(){
+    //Given:Setup base url
+    String baseUrl = "https://jsonplaceholder.typicode.com";
+
+    //When: Make GET request
+    Response response = given()
+            .baseUri(baseUrl)
+            .when()
+            .get("/users")
+            .then()
+            .statusCode(200)
+            .body("size()", equalTo(10))
+            .extract()
+            .response();
+
+    //print response
+    System.out.println("Response: "+response.asString());
+  }
+
+}
